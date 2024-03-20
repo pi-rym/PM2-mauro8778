91c6d63f0422a2d2a07dcd925a6a77546ec5397c
MDiaz8778 <68826380+mauro8778@users.noreply.github.com>
2024-03-20 15:38:28 -0300

    asasd

---
+++ .history/front/scripts/LimpiarFormulario_20240320121747.js
@@ -0,0 +1,71 @@
+
+const Genres= ['drama','Action','Fantasy','Comedy','Terror','Romance','Sci-Fi','Adventure'];
+function limpiarFormulario() {
+    document.querySelectorAll('input').forEach(input => input.value = ' ');
+}
+function validar(event){
+
+    event.preventDefault();
+
+    const tituloinput =document.getElementById("title");
+    const directorinput= document.getElementById("director");
+    const durationinput= document.getElementById("duration");
+    const añoinput= document.getElementById("year");
+    const generoinput= document.getElementById("genre");
+    const puntuacioninput= document.getElementById("rate");
+    const imageninput= document.getElementById("poster");
+
+    const titulo = tituloinput.value;
+    const director = directorinput.value;
+    const duracion = durationinput.value;
+    const año = añoinput.value;
+    const genero = generoinput.value;
+    const puntuacion = puntuacioninput.value;
+    const imagen = imageninput.value;
+
+
+    
+
+renderGenres();
+
+const  validarcheck(){
+
+    const checkbocks = document.getElementById("genre")
+
+
+}
+    if(titulo ===''||director ===''||duracion ===''|| año ===''|| genero ===''||puntuacion===''||imagen===''){
+
+        alert("porfavor completar los campos");
+        return;
+    }
+    else{
+
+        this.submit();
+    }
+    
+}
+function renderGenres(){
+        
+    genre.innerHTML=''
+for(const genre of genres){
+
+    const input = document.createElement('input')
+    const label = document.createElement('label')
+    input.type ="checkbox"
+    input.id = genre
+    input.name = "genre[]"
+    input.value = genre
+
+    label.htmlFor =genre
+    label.textContent = genre
+
+    genre.appendchild(input)
+    genre.appendchild(label)
+}
+
+return genre;
+}
+document.querySelector('form').addEventListener('submit', validar);
+
+module.exports = {limpiarFormulario, renderGenres};
---
+++ .history/front/scripts/enviarFormulario_20240320124156.js
@@ -0,0 +1,26 @@
+const axios = require('axios');
+
+async function enviarFormulario() {
+    const movieData = {
+        title: document.getElementById('title').value,
+        director: document.getElementById('director').value,
+        duration: document.getElementById('duration').value,
+        year: document.getElementById('year').value,
+        rate: document.getElementById('rate').value,
+        genre: document.getElementById('genre').value.split(',').map(item => item.trim());
+        poster: document.getElementById('poster').value
+    };
+
+    try {
+        const response = await axios.post('http://localhost:3000/movies', movieData);
+        if (response.status === 201) {
+            console.log('Película creada exitosamente:', response.data);
+        } else {
+            console.error('Error al crear la película:', response.statusText);
+        }
+    } catch (error) {
+        console.error('Error al enviar los datos:', error);
+    }
+}
+
+module.exports = enviarFormulario;
